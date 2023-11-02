223f00a1fad131a8a9eadc853acd46d5a90bdb27
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-11-02 10:02:27 -0600

    Discover a new analysis edge-case

---
+++ src/analyzer/demos/9_fail.rs
@@ -0,0 +1,38 @@
+fn main() {
+    a();
+    b();
+}
+
+fn a() {
+    borrow_mutably::<u32>();
+}
+
+fn b() {
+    unborrow_mutably::<u32>();
+    borrow_mutably::<u32>();
+    borrow_mutably::<u32>();
+}
+
+fn borrow_mutably<T: ?Sized>() {
+    fn __autoken_borrow_mutably<T: ?Sized>() {}
+
+    __autoken_borrow_mutably::<T>();
+}
+
+fn borrow_immutably<T: ?Sized>() {
+    fn __autoken_borrow_immutably<T: ?Sized>() {}
+
+    __autoken_borrow_immutably::<T>();
+}
+
+fn unborrow_mutably<T: ?Sized>() {
+    fn __autoken_unborrow_mutably<T: ?Sized>() {}
+
+    __autoken_unborrow_mutably::<T>();
+}
+
+fn unborrow_immutably<T: ?Sized>() {
+    fn __autoken_unborrow_immutably<T: ?Sized>() {}
+
+    __autoken_unborrow_immutably::<T>();
+}
