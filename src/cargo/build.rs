9dbe78ce79378ff4c751c9ce8a153f67cb1c3312
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-11-05 15:35:38 -0700

    Implement a slightly more robust `cargo` wrapper

---
+++ src/cargo/build.rs
@@ -0,0 +1,67 @@
+use std::{path::PathBuf, process::Command};
+
+fn main() -> anyhow::Result<()> {
+    // Don't rebuild this crate when nothing changed.
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Extract some environment variables.
+    let rustc_exe = PathBuf::from(std::env::var("RUSTC")?);
+    let cargo_exe = PathBuf::from(std::env::var("CARGO")?);
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
+
+    // Make `EXPECTED_RUSTC_VERSION` available to the binary.
+    let rustc_version =
+        String::from_utf8(Command::new(rustc_exe).arg("--version").output()?.stdout)?;
+
+    println!("cargo:rustc-env=AUTOKEN_EXPECTED_RUSTC_VERSION={rustc_version}");
+
+    // Ensure that our rustc wrapper is also available for embedding.
+    let rustc_wrapper_src = {
+        // `./../rustc`
+        let mut cwd = std::env::current_dir().unwrap();
+        cwd.pop();
+        cwd.push("rustc");
+        cwd
+    };
+
+    println!(
+        "cargo:rerun-if-changed={}",
+        rustc_wrapper_src.to_string_lossy()
+    );
+
+    let build_result = Command::new(cargo_exe)
+        .current_dir(rustc_wrapper_src)
+        .arg("build")
+        .arg("-Z")
+        .arg("unstable-options")
+        .arg("--release")
+        .arg("--out-dir")
+        .arg(&out_dir)
+        .spawn()?
+        .wait()?;
+
+    anyhow::ensure!(build_result.success(), "failed to build the rustc wrapper");
+
+    let binary_path = {
+        let mut out = out_dir.clone();
+        out.push("out");
+        if cfg!(windows) {
+            out.set_file_name("autoken-rustc.exe");
+        } else {
+            out.set_file_name("autoken-rustc");
+        }
+        out
+    };
+
+    println!(
+        "cargo:rustc-env=AUTOKEN_RUSTC_WRAPPER_BINARY={}",
+        binary_path.to_string_lossy()
+    );
+
+    println!(
+        "cargo:rustc-env=AUTOKEN_RUSTC_WRAPPER_BINARY_HASH={}",
+        sha256::digest(std::fs::read(binary_path)?),
+    );
+
+    Ok(())
+}
--- src/cargo/src/main.rs
+++ src/cargo/src/main.rs
@@ -1,88 +1,176 @@
-use std::process::Command;
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use anyhow::Context;
 use directories::ProjectDirs;
 use rustc_build_sysroot::{SysrootBuilder, SysrootConfig};
 
 fn main() -> anyhow::Result<()> {
-    // We try to stuff two binaries in one: `cargo-autoken` and `autoken-rustc`. This code is what
-    // allows us to do that.
-    //
-    // If the `AUTOKEN_CARGO_ACT_AS_RUSTC` environment variable is set, act as `autoken-rustc`.
-    // Otherwise, we're running as `cargo autoken`!
-    if std::env::var("AUTOKEN_CARGO_ACT_AS_RUSTC_WRAPPER").is_ok_and(|v| v == "yes") {
-        autoken_rustc::analyzer::main_inner(std::env::args().collect()); // This is divergent.
+    // First, we must ensure that we are being run in the context of cargo.
+    let (cargo_exe, cargo_args) = extract_cargo_tool_params()
+        .context("this binary must be executed as a cargo tool via `cargo autoken`")?;
+
+    // Next, ensure that cargo's `rustc` version string against which `cargo`'s linker path is
+    // provided is appropriate for our rustc binary.
+    let mut cargo_rustc_exe = cargo_exe.clone();
+    if cfg!(windows) {
+        cargo_rustc_exe.set_file_name("rustc.exe");
+    } else {
+        cargo_rustc_exe.set_file_name("rustc");
+    }
+    let cargo_rustc_version = rustc_version_str(&cargo_rustc_exe).context(
+        "failed to determine version of the rustc binary with which the invoking cargo command was \
+         distributed",
+    )?;
+
+    #[allow(clippy::comparison_to_empty)] // false positive
+    if cargo_rustc_version.lines().next() != env!("AUTOKEN_EXPECTED_RUSTC_VERSION").lines().next() {
+        anyhow::bail!(
+            "The version of rustc with which cargo was bundled was {:?} but autoken's \
+             rustc version was {:?}. Make sure to call cargo with the appropriate toolchain parameter.",
+            cargo_rustc_version,
+            env!("AUTOKEN_EXPECTED_RUSTC_VERSION"),
+        );
     }
 
-    // Parse the arguments the user wants to send to cargo.
-    let in_args = {
-        let mut in_args = std::env::args();
+    // Now, create a directory to store all our work.
+    let app_dir = ProjectDirs::from("me", "radbuglet", "autoken")
+        .context("failed to get app-dir for autoken")?;
+    let app_dir = app_dir.cache_dir();
 
-        // Skip the current binary argument
-        in_args.next();
+    // Now, we need to unpack our custom version of `rustc` into a file where we can execute it.
+    let rustc_wrapper_path = {
+        let mut path = app_dir.to_path_buf();
 
-        // Skip the `autoken` argument in `cargo_autoken autoken <...>`.
-        if in_args.next().as_deref() != Some("autoken") {
-            anyhow::bail!("this binary should be called through cargo via `cargo autoken`");
+        if cfg!(windows) {
+            path.push("autoken_rustc_wrapper.exe");
+        } else {
+            path.push("autoken_rustc_wrapper");
         }
-        in_args
+        path
     };
+    write_rustc_wrapper_exe(&rustc_wrapper_path).context("failed to create rustc wrapper file")?;
 
-    // We're going to be calling cargo with a special version of `RUSTC`.
-    let cargo_exe = std::env::var("CARGO").context("no cargo binary provided")?;
-    let current_exe = std::env::current_exe()?;
-    let make_cargo_cmd = || {
-        let mut cmd = Command::new(&cargo_exe);
-        cmd.env("RUSTC", &current_exe);
-        cmd.env("AUTOKEN_CARGO_ACT_AS_RUSTC_WRAPPER", "yes");
+    let rustc_cmd = |skip_analysis: bool, sysroot: Option<&Path>| {
+        let mut cmd = Command::new(&rustc_wrapper_path);
+
+        if skip_analysis {
+            cmd.env("AUTOKEN_SKIP_ANALYSIS", "yes");
+        } else {
+            cmd.env_remove("AUTOKEN_SKIP_ANALYSIS");
+        }
+
+        if let Some(sysroot) = sysroot {
+            cmd.env("AUTOKEN_OVERRIDE_SYSROOT", sysroot);
+        } else {
+            cmd.env_remove("AUTOKEN_OVERRIDE_SYSROOT");
+        }
+
         cmd
     };
 
-    // We're about to rerun cargo with some special environment variables but we have to make sure
-    // that the cargo binary we're about to execute is appropriate for our toolchain.
-    // TODO
+    let cargo_cmd = |rust_cmd: Command| {
+        let mut cmd = Command::new(&cargo_exe);
+        cmd.env("RUSTC", rust_cmd.get_program());
+        cmd.envs(rust_cmd.get_envs().filter_map(|(a, b)| Some((a, b?))));
+        cmd
+    };
 
-    // We need to determine the target with which the user wants us to build this project.
-    let target_triple = "aarch64-apple-darwin"; // TODO
+    // Let's also ensure that we have a valid sysroot for this version of `rustc`.
+    build_sysroot(
+        app_dir,
+        // TODO: Determine target dynamically
+        "aarch64-apple-darwin",
+        rustc_cmd(true, None),
+        cargo_cmd(rustc_cmd(true, None)),
+    )
+    .context("failed to build sysroot")?;
 
-    // `autoken-rustc` has special requirements on how the standard library is built so let's create
-    // a custom sysroot for that if it doesn't already exist.
-    let sysroot_store = ProjectDirs::from("me", "radbuglet", "autoken")
-        .context("failed to get sysroot cache directory")?;
-    let sysroot_store = sysroot_store.cache_dir();
-    {
-        // Determine the path to the source code for the sysroot compilation
-        let sysroot_src_code = rustc_build_sysroot::rustc_sysroot_src({
-            let mut cmd = Command::new(&current_exe);
-            cmd.env("AUTOKEN_CARGO_ACT_AS_RUSTC_WRAPPER", "yes");
-            cmd
-        })?;
+    // Now that all of that is out of the way, we can finally re-dispatch cargo with our custom rustc
+    // version.
+    std::process::exit(
+        cargo_cmd(rustc_cmd(false, Some(app_dir)))
+            .args(cargo_args)
+            .spawn()
+            .context("failed to spawn cargo")?
+            .wait_with_output()?
+            .status
+            .code()
+            .unwrap_or(1),
+    );
+}
 
-        if !sysroot_src_code.exists() {
-            anyhow::bail!("could not find rust-src for this current toolchain");
-        }
+fn extract_cargo_tool_params() -> anyhow::Result<(PathBuf, Vec<String>)> {
+    let cargo_exe = std::env::var("CARGO").context("`CARGO` environment variable was not set")?;
 
-        // Create it!
-        SysrootBuilder::new(sysroot_store, target_triple)
-            .cargo({
-                let mut cmd = make_cargo_cmd();
-                cmd.env("AUTOKEN_SKIP_ANALYSIS", "yes");
-                cmd
-            })
-            .sysroot_config(SysrootConfig::WithStd {
-                std_features: vec!["panic_unwind".to_string(), "backtrace".to_string()],
-            })
-            .build_from_source(&sysroot_src_code)?;
+    let mut cargo_args = std::env::args();
+
+    // Skip the current binary argument
+    cargo_args.next();
+
+    // Skip the `autoken` argument in `cargo_autoken autoken <...>`.
+    if cargo_args.next().as_deref() != Some("autoken") {
+        anyhow::bail!("this binary should be called through cargo via `cargo autoken`");
     }
 
-    // With all the validation out of the way, we can now run cargo. Our only requirement is that
-    // `RUSTC` and the sysroot are appropriately set.
-    let mut cmd = make_cargo_cmd();
-    cmd.env("RUSTC_AUTOKEN_OVERRIDE_SYSROOT", sysroot_store);
-    cmd.args(in_args);
-    cmd.spawn()
-        .context("failed to spawn final cargo command")?
-        .wait()?;
+    Ok((PathBuf::from(cargo_exe), cargo_args.collect()))
+}
+
+fn rustc_version_str(rustc: &Path) -> anyhow::Result<String> {
+    Ok(String::from_utf8(
+        Command::new(rustc).arg("--version").output()?.stdout,
+    )?)
+}
+
+fn write_rustc_wrapper_exe(path: &Path) -> anyhow::Result<()> {
+    // Ensure that the parent directory exists
+    fs::create_dir_all(path.parent().unwrap())?;
+
+    // Set the file options
+    let mut opts = File::options();
+    opts.write(true).create(true).truncate(true);
+
+    // Write the data to the file
+    let mut rust_wrapper_file = opts.open(path)?;
+    rust_wrapper_file.write_all(rustc_wrapper_data())?;
+
+    #[cfg(unix)]
+    rust_wrapper_file.set_permissions({
+        use std::os::unix::fs::PermissionsExt;
+        let mut perms = rust_wrapper_file.metadata()?.permissions();
+        perms.set_mode(perms.mode() | 0o111);
+        perms
+    })?;
+
+    Ok(())
+}
+
+fn rustc_wrapper_data() -> &'static [u8] {
+    include_bytes!(env!("AUTOKEN_RUSTC_WRAPPER_BINARY"))
+}
+
+fn build_sysroot(
+    store_path: &Path,
+    target: &str,
+    rust_cmd: Command,
+    cargo_cmd: Command,
+) -> anyhow::Result<()> {
+    let sysroot_src_code = rustc_build_sysroot::rustc_sysroot_src(rust_cmd)?;
+
+    if !sysroot_src_code.exists() {
+        anyhow::bail!("could not find rust-src for this current toolchain");
+    }
+
+    SysrootBuilder::new(store_path, target)
+        .cargo(cargo_cmd)
+        .sysroot_config(SysrootConfig::WithStd {
+            std_features: vec!["panic_unwind".to_string(), "backtrace".to_string()],
+        })
+        .build_from_source(&sysroot_src_code)?;
 
     Ok(())
 }
