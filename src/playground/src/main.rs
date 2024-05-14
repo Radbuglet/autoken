0755f45e78e15b30c5999576028891c7b8439839
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2024-05-14 08:35:15 +0200

    Find an exciting new soundness hole

--- src/playground/src/main.rs
+++ src/playground/src/main.rs
@@ -1,50 +1,16 @@
 fn main() {}
 
-fn works<'a>() -> impl Iterator<Item = &'a ()> {
-    let v = (0..1).map(move |_| &());
-    v
-}
-
-fn previously_broke<'a>() -> impl Iterator<Item = &'a ()> {
-    (0..1).map(move |_| &())
-}
-
-fn still_breaks<'a>() -> impl Iterator<Item = &'a ()> {
-    previously_broke()
-}
-
-fn woo() {
-    whee(|| {
-        tie();
-    });
-}
-
-fn whee(f: impl FnOnce()) {
-    let guard = tie();
-    f();
-    let _ = guard;
-}
-
-fn hehe(_dummy: &u32) -> (&u32, &str) {
-    map(tie())
-}
-
-fn hehe2<'b, 'c>() -> (&'b u32, &'c str) {
-    map(tie())
-}
-
-fn hehe3<'b, 'c>() -> (&'b u32, &'c str) {
-    autoken::tie!('b => mut ());
-    autoken::tie!('c => mut ());
-
-    map(tie())
-}
-
-fn map(_v: &()) -> (&u32, &str) {
-    (&3, "hi!!")
-}
-
 fn tie<'a>() -> &'a () {
     autoken::tie!('a => mut ());
     &()
+}
+
+fn huh<'a, R>(f: impl FnOnce(&'a ()) -> R) -> R {
+    f(tie())
+}
+
+fn wuh() {
+    let a = huh(|v| v);
+    let _ = huh(|v| v);
+    let _ = a;
 }
