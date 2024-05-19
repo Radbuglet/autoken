ec5d302b4f4866027e569223e9ebd8b4ff561010
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2024-05-19 09:49:38 +0200

    Implement `save_to_file` and `save_from_file`

--- src/rustc/src/lib.rs
+++ src/rustc/src/lib.rs
@@ -8,6 +8,7 @@ extern crate rustc_data_structures;
 extern crate rustc_driver;
 extern crate rustc_hash;
 extern crate rustc_hir;
+extern crate rustc_incremental;
 extern crate rustc_index;
 extern crate rustc_infer;
 extern crate rustc_interface;
