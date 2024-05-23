5e4f19db14ae87d1c33a7953a6912c95877c3db1
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2024-05-23 21:40:54 +0200

    Clean playground

--- src/playground/src/main.rs
+++ src/playground/src/main.rs
@@ -1,18 +1,1 @@
-use autoken::Brand;
-use std::{pin::pin, future::{Future, pending}};
-
 fn main() {}
-
-fn wazoo() {
-    let _ = pin!(foo()).poll(&mut todo());
-}
-
-async fn foo() {
-    let brand = Brand::<u32>::acquire_mut();
-    pending::<()>().await;
-    let _ = brand;
-}
-
-fn todo<T>() -> T {
-    todo!();
-}
